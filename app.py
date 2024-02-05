cb553cf581e8746bc6f3859015de509865dd46ff
Ayush <97424352+carpx05@users.noreply.github.com>
2024-02-05 22:22:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,15 @@ import pandas as pd
 import re
 import base64
 from PyPDF2 import PdfMerger
+import os
 
 st.header('Invoice Data Analysis')
+session_state = st.session_state
+if 'df' not in session_state:
+    session_state.df = None  
+
+if 'result_df' not in session_state:
+    session_state.result_df = None
 
 @st.cache_data
 def calculate_similarity(address1, address2):
@@ -20,9 +27,9 @@ def preprocess_text(text):
     cleaned_text = ' '.join(set(cleaned_text.split()))
     return cleaned_text
 
-@st.cache_data
-def prepare_df(pdf_file, crop_down=120, crop_lr=130, crop_down_inv=50, crop_lr_inv=50):
-    doc = fitz.open(pdf_file)
+@st.cache_data(show_spinner="Fetching data")
+def prepare_df(pdf_file_path, crop_down=120, crop_lr=130, crop_down_inv=50, crop_lr_inv=50):
+    doc = fitz.open(pdf_file_path)
     df = pd.DataFrame(columns=["invoice_num", "invoice_date", "customer_address", "page_num"])
 
     for page_num in range(doc.page_count):
@@ -74,7 +81,7 @@ def download_link(df, filename='dataframe.csv', text='Download CSV'):
     href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">{text}</a>'
     return href
 
-@st.cache_data
+@st.cache_data(show_spinner="Fetching data")
 def find_most_similar_address(query, addresses, threshold=60):
     result = process.extractOne(query, addresses)
     if result[1] >= threshold:
@@ -93,45 +100,77 @@ def merge_pdfs(uploaded_files):
     merger.close()
     return merged_file_path
 
+@st.cache_data
+def save_uploadedfile(uploadedfile):
+    if uploadedfile:
+        directory = "tempDir"
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        file_path = os.path.join(directory, uploadedfile.name)
+        with open(file_path,"wb") as f:
+            f.write(uploadedfile.getbuffer())
+        st.success("Saved File:{} to tempDir".format(uploadedfile.name))
+        return file_path
+    
+def save_merged_file(file):
+    if file:
+        directory = "tempDir"
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        file_path = os.path.join(directory, file)
+        with open(file_path,"wb") as f:
+            f.write(file)
+        st.success("Saved File:{} to tempDir".format(file))
+        return file_path
+
+
 def main():
+    uploaded_file_path = None 
     upload_option = st.selectbox("Choose upload option:", ["Single file", "Multiple files"])
+    df = None
+    uploaded_file = None
+    result_df = pd.DataFrame()
 
     # File upload based on selected option
     if upload_option == "Single file":
         uploaded_file = st.file_uploader("Upload a single PDF file", type="pdf", accept_multiple_files=False)
+        uploaded_file_path = save_uploadedfile(uploaded_file)
+
     else:
         uploaded_files = st.file_uploader("Upload multiple PDF files", accept_multiple_files=True, type="pdf")
-        uploaded_file = merge_pdfs(uploaded_files)
-        st.success("PDFs merged successfully!")
-        st.download_button(label="Download Merged PDF", data=open(uploaded_file, "rb").read(), file_name="merged_file.pdf", mime="application/pdf")
+        if uploaded_files:
+            uploaded_file = merge_pdfs(uploaded_files)
+            print("uploaded_file:",uploaded_file)
+            st.success("PDFs merged successfully!")
+            uploaded_file_path = uploaded_file
+            st.download_button(label="Download Merged PDF", data=open(uploaded_file, "rb").read(), file_name="merged_file.pdf", mime="application/pdf")
 
+    if uploaded_file_path:
+        df = prepare_df(uploaded_file_path)
 
-    df = prepare_df(uploaded_file)
-    # pdf_file = "example.pdf"
-    # crop_down = 120
-    # crop_lr = 120
-    # df = prepare_df(pdf_file, crop_down, crop_lr)
+    if st.button('View Analysis'):
+        print(df)
+        if df is not None:  # Check if df is assigned
+        # Proceed with DataFrame operations
 
-    if st.button('View Scanned Data'):
-        st.write('You clicked the button! Here is your DataFrame:')
-        st.dataframe(df)
-        print("df 1 ready!")
+            st.write('Here is your DataFrame, Scanned Data')
+            st.dataframe(df)
+            print("df 1 ready!")
 
-        st.markdown(download_link(df, filename='my_dataframe.csv', text='Download CSV'), unsafe_allow_html=True)
+            st.markdown(download_link(df, filename='my_dataframe.csv', text='Download CSV'), unsafe_allow_html=True)
 
-    df['most_similar_address'] = df['customer_address'].apply(lambda x: find_most_similar_address(x, df['customer_address']))
+            df['most_similar_address'] = df['customer_address'].apply(lambda x: find_most_similar_address(x, df['customer_address']))
+            df_filtered = df.dropna(subset=['most_similar_address'])
+            address_counts = df_filtered.groupby('most_similar_address').size().reset_index(name='count')
+            result_df = pd.merge(address_counts, df[['most_similar_address', 'page_num']], on='most_similar_address', how='left')
+            result_df['page_numbers'] = result_df.groupby('most_similar_address')['page_num'].transform(lambda x: ', '.join(map(str, x)))
+            result_df.drop_duplicates(subset=['most_similar_address'], inplace=True)
 
-    df_filtered = df.dropna(subset=['most_similar_address'])
-    address_counts = df_filtered.groupby('most_similar_address').size().reset_index(name='count')
-    result_df = pd.merge(address_counts, df[['most_similar_address', 'page_num']], on='most_similar_address', how='left')
-    result_df['page_numbers'] = result_df.groupby('most_similar_address')['page_num'].transform(lambda x: ', '.join(map(str, x)))
-    result_df.drop_duplicates(subset=['most_similar_address'], inplace=True)
-
-    if st.button('View Similar Addresses'):
-        st.write('You clicked the button! Here is your DataFrame:')
-        st.dataframe(result_df)
-        st.write('page_numbers refer to the page  number of the invoice in the merged pdf. Download Merger PDF to view.')
-        print("df 2 ready!")
+            st.write('Here is your DataFrame, Similar Customer Addresses')
+            result_df=session_state.result_df
+            st.dataframe(result_df)
+            st.write('page_numbers refer to the page  number of the invoice in the merged pdf. Download Merger PDF to view.')
+            print("df 2 ready!")
 
 if __name__ == "__main__":
     main()
